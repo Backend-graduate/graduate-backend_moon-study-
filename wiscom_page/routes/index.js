afc6e12b2145d27aab6e6641fbf48ac063e104c5
devMooon <anstjdus0702@duksung.ac.kr>
2022-09-12 13:19:51 +0900

    기본 라우터 연결

--- wiscom_page/routes/index.js
+++ wiscom_page/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/developers', function(req, res, next) {
+  res.render('./developers', { title: 'Developers' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('./projects', { title: 'Projects' });
+});
+
 module.exports = router;
